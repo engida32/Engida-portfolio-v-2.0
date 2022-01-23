f6c5ab34b3718c5a4a20dff72c80fbe9d036c8f6
engida32 <engida32lishan@gmail.com>
2022-01-24 00:38:58 +0300

    added about component

---
+++ src/components/About/About.tsx
@@ -0,0 +1,89 @@
+import { Container } from "./styles";
+
+import profileImage from "../../assets/engida-pic.JPG"
+import cssIcon from "../../assets/css-icon.svg";
+import htmlIcon from "../../assets/html-icon.svg";
+import jsIcon from "../../assets/js-icon.svg";
+import nodeIcon from "../../assets/node-icon.svg";
+import reactIcon from "../../assets/react-icon.svg";
+import typescriptIcon from "../../assets/typescript-icon.svg";
+import vueIcon from "../../assets/vue-icon.svg";
+import ScrollAnimation from "react-animate-on-scroll";
+
+
+export function About() {
+  return (
+    <Container id="about">
+      <div className="about-text">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <h2>About me</h2>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000}>
+          <p>My name is Engida Lishan, I am a software Engineer with a great passion for programming.I am passionate about delivering solutions that aggregate into people's lives and at the same time challenge me.I improved my skills as a developer Front-End e Back-End</p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+
+          <p>Develop websites and applications using HTML, CSS, and JavaScript.I have familiarity in developing layouts that supply me.I am always improving every project that put in my hands.</p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.6 * 1000}>
+
+          <p>I am a dedicated person who runs behind to realize his dreams, worker and results oriented, I always seek to reach my best version.</p>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.7 * 1000}>
+          <h3>Here are my main skills:</h3>
+        </ScrollAnimation>
+        <div className="hard-skills">
+
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.1 * 1000}>
+              <img src={reactIcon} alt="React" />
+            </ScrollAnimation>
+          </div>
+
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.2 * 1000}>
+              <img src={vueIcon} alt="Vue" />
+            </ScrollAnimation>
+          </div>
+
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.3 * 1000}>
+              <img src={jsIcon} alt="JavaScript" />
+            </ScrollAnimation>
+          </div>
+
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.4 * 1000}>
+              <img src={htmlIcon} alt="Html" />
+            </ScrollAnimation>
+          </div>
+
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.5 * 1000}>
+              <img src={cssIcon} alt="Css" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}>
+              <img src={nodeIcon} alt="Node" />
+            </ScrollAnimation>
+          </div>
+
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.7 * 1000}>
+              <img src={typescriptIcon} alt="Typescript" />
+            </ScrollAnimation>
+          </div>
+
+
+        </div>
+      </div>
+      <div className="about-image">
+        <ScrollAnimation animateIn="fadeInRight" delay={0.6 * 1000}>
+          <img src={profileImage} alt="Imagem de perfil" />
+        </ScrollAnimation>
+      </div>
+    </Container>
+  )
+}
