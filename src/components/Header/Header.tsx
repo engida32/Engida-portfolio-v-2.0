93d9e00ad1e86ea5ff7d02e391b5bb7c9e9b2fe9
engida32 <engida32lishan@gmail.com>
2022-01-24 00:38:23 +0300

    added header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,64 @@
+import { Container } from './styles'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { NavHashLink, HashLink } from 'react-router-hash-link'
+import { useState } from 'react'
+
+import CurriculumV from '../../assets/ EngidaLishanGashaw Resume.pdf'
+export function Header() {
+  const [isActive, setActive] = useState(false)
+
+  function toggleTheme() {
+    let html = document.getElementsByTagName('html')[0]
+    html.classList.toggle('light')
+  }
+
+  function closeMenu() {
+    setActive(false)
+  }
+
+  return (
+    <Container className="header-fixed">
+      <Router>
+        <HashLink smooth to="#home" className="logo">
+          <span>Home</span>
+        </HashLink>
+
+        <input
+          onChange={toggleTheme}
+          className="container_toggle"
+          type="checkbox"
+          id="switch"
+          name="mode"
+        />
+        <label htmlFor="switch">Toggle</label>
+
+        <nav className={isActive ? 'active' : ''}>
+          <NavHashLink smooth to="#home" onClick={closeMenu}>
+            Home
+          </NavHashLink>
+          <NavHashLink smooth to="#about" onClick={closeMenu}>
+            About me          </NavHashLink>
+          <NavHashLink smooth to="#portfolio" onClick={closeMenu}>
+            Portfolio
+          </NavHashLink>
+          <NavHashLink smooth to="#contact" onClick={closeMenu}>
+            Contact
+          </NavHashLink>
+          <a href={CurriculumV} download className="button">
+            CV
+          </a>
+        </nav>
+
+        <div
+          aria-expanded={isActive ? 'true' : 'false'}
+          aria-haspopup="true"
+          aria-label={isActive ? 'Fechar menu' : 'Abrir menu'}
+          className={isActive ? 'menu active' : 'menu'}
+          onClick={() => {
+            setActive(!isActive)
+          }}
+        ></div>
+      </Router>
+    </Container>
+  )
+}
